432aee6a9fc4dc1dcc240cb7fd07ac55bb5c2cf3
Codrin Socol <82222825+CodrinSocol@users.noreply.github.com>
2024-08-08 14:43:44 +0200

    skeleton app for backend (#8)
    * skeleton app for backend
    
    * created a CI workflow that builds client & server
    
    * small change to fix server build
    
    * retry with java version 22
    
    * readded distribution

---
+++ server/src/test/java/com/bvb/bvbdividends/BvbDividendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvb.bvbdividends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BvbDividendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
