099f56a992951fba5d208b179b170eeb12d9e930
Mihnea-Andrei BLOȚIU <mihnea.blotiu@stud.acs.upb.ro>
2024-08-13 21:14:18 +0300

    refresh_secrets.sh update

--- server/env/refresh_secrets.sh
+++ server/env/refresh_secrets.sh
@@ -5,6 +5,24 @@ RESOLVED_DIR="env/resolved"
 
 rm -rf env/resolved/*
 
+install_hcp() {
+    echo "hcp not found. Installing hcp CLI..."
+
+    sudo apt-get update && sudo apt-get install wget gpg coreutils
+    wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+    echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+    sudo apt-get update && sudo apt-get install hcp
+
+    echo "hcp CLI installed successfully."
+}
+
+if ! command -v hcp &> /dev/null; then
+    install_hcp
+fi
+
+hcp auth login
+hcp profile init --vault-secrets
+
 for template_file in "${TEMPLATES_DIR}"/*; do
     base_template_file_name=$(basename "${template_file}")
     resolved_file_name="${RESOLVED_DIR}"/"${base_template_file_name}"
