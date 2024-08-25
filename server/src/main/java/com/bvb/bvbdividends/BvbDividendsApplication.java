77dcd71720936474d16c2d647df8253bfa942d8e
CodrinSocol <codrin.socol@gmail.com>
2024-08-25 15:05:35 +0200

    set up cron infra & daily noon trigger

--- server/src/main/java/com/bvb/bvbdividends/BvbDividendsApplication.java
+++ server/src/main/java/com/bvb/bvbdividends/BvbDividendsApplication.java
@@ -3,8 +3,10 @@ package com.bvb.bvbdividends;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@EnableScheduling
 public class BvbDividendsApplication {
 
 	public static void main(String[] args) {
